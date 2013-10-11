fdc365d4012342ffe7efeb7c3273f7e0561ec295
Angela Zhang <zhangela@mit.edu>
2013-10-11 18:43:15 -0400

    Added logic to get all events from one calendar. TODO: get events from all calendars.

--- client/index.js
+++ client/index.js
@@ -17,7 +17,7 @@ Template.calendar.days = function () {
 var calendarList = function() {
     Meteor.call('getCalendarList', function(err, response) {
         Session.set('serverCalendarListResponse', response);
-        if (response['data'] != null && response['data']['item'] != null) { 
+        try {
             var calendarListResponse = response['data']['items'];
             var calendars = calendarListResponse.map(function(item) {
                 return {
@@ -26,6 +26,8 @@ var calendarList = function() {
                 };
             })
             Session.set('calendarList', calendars);
+        } catch (err) {
+            console.log(err);
         }
     });
 };
@@ -38,7 +40,14 @@ Template.index_loggedin.calendars = function () {
 
 
 Template.index_loggedin.freetime = function () {
-    var timespan = 7
+    var timespan = 7;
+    var calendars = Session.get('calendarList');
+    Meteor.call('getFreeTime', timespan, calendars, function(err, response) {
+        if (response != null) {
+            console.log(response);
+            Session.set('serverFreeTimeResponse', response);                
+        }
+    });
 
     
 };
--- server/server.js
+++ server/server.js
@@ -21,6 +21,46 @@ Meteor.startup(function() {
             );
 
             return future.wait();
+        },
+
+        getFreeTime: function(timespan, calendars) {
+
+            console.log('getFreeTime called');
+            this.unblock();
+
+            var future = new Future();
+            var events = null;
+
+            for (var i = 0; i < calendars.length; i++) {
+                calendar_id = calendars[i].id;
+                console.log(calendar_id);
+                console.log(calendar_id.replace("@", "%40"));
+
+                Meteor.http.get("https://www.googleapis.com/calendar/v3/calendars/"
+                    + calendar_id.replace("@", "%40")
+                    + "/events?maxResults=1000"
+                    + "&singleEvents=true&timeMax=" 
+                    + "2013-10-18T00%3A00%3A00%2B00%3A00&"
+                    + "timeMin=2013-10-01T00%3A00%3A00%2B00%3A00"
+                    + "&key=" 
+                    + Meteor.settings.public.google_calendar_api_key,
+                    { headers: { "Authorization": "Bearer " + Meteor.user().services.google.accessToken}
+                    }, 
+                    function(error, result) {
+                        events = result['data']['items'].map(function(item) {
+                            return {
+                                summary: item.summary,
+                                id: item.id,
+                                start: item.start.dateTime,
+                                end: item.end.dateTime
+                            };
+                        });
+                        future.return(events); //how do i make it return events in all calendars
+                    }
+                );
+            }
+            return future.wait();
+
         }
     });
 });
